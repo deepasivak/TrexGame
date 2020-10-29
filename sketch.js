7039736a17176b81fba6e30ae3b9324725d7e5d8
deepasivak <73427458+deepasivak@users.noreply.github.com>
2020-10-29 15:38:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -94,11 +94,11 @@ function draw() {
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
     
-    /*if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
+    if((touches.length > 0 || keyDown("SPACE")) && trex.y  >= height-120) {
       jumpSound.play( )
       trex.velocityY = -10;
        touches = [];
-    }*/
+    }
     
     trex.velocityY = trex.velocityY + 0.8
   
